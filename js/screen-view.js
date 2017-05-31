f7ada81c901586ded67b4f9255457a24b2dea86c
Valeriy Grigoryev <valeriy.grigoryev@gmail.com>
2017-05-31 15:59:05 +0300

    Удалил закомментированные участки кода

--- js/screen-view.js
+++ js/screen-view.js
@@ -4,24 +4,7 @@
 export default class ScreenView {
   constructor() {
     this.mainScreen = document.querySelector(`.main`);
-/*    const START_INDEX = 0;
-
-    this.mainScreen = document.querySelector(`.main`);
-    this.screens = [welcomScreen, artistScreen, genreScreen, successScreen, failScreen];
-
-    this.mainScreen.appendChild(this.screens[START_INDEX]());
-    this.currentIndex = START_INDEX;*/
   }
-
-/*
-  showScreen(screenId) {
-    const prevChild = this.mainScreen.querySelectorAll(`*`)[0];
-
-    this.currentIndex = screenId;
-
-    this.mainScreen.replaceChild(this.screens[this.currentIndex](), prevChild);
-  }
-  */
 
   showScreen(screenFragment) {
     const prevChild = this.mainScreen.querySelectorAll(`*`)[0];
