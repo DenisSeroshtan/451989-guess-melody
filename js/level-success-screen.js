932f948548aa3d75b7f9a1e40b8a80ed2f79e151
Valeriy Grigoryev <valeriy.grigoryev@gmail.com>
2017-06-05 17:45:59 +0300

    Промежуточный коммит

--- js/level-success-screen.js
+++ js/level-success-screen.js
@@ -4,13 +4,15 @@
 import convertToHtml from './string-to-html.js';
 import main from './main.js';
 import welcomeScreen from './welcome-screen.js';
+import * as gameData from './data.js';
+
+const LOGO = `<section class="logo" title="${gameData.gameInfo.gameName}"><h1>${gameData.gameInfo.gameName}</h1></section>`;
 
 const screenElement = `<section class="main main--result">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-
+    ${LOGO}
     <h2 class="title">Вы настоящий меломан!</h2>
-    <div class="main-stat">За&nbsp;2&nbsp;минуты<br>вы&nbsp;отгадали 4&nbsp;мелодии</div>
-    <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
+    <div class="main-stat">За&nbsp;${Math.round(gameData.initGameState.time / 60)}&nbsp;минуты<br>вы&nbsp;отгадали ${gameData.initGameState.correctAnswers}&nbsp;мелодии</div>
+    <span class="main-comparison">Это&nbsp;лучше чем у&nbsp;${gameData.getPercentHighscore()}&nbsp;игроков</span>
     <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
   </section>`;
 
