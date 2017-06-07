4d8d6f9d849b3f4f7c65a0e52a52d998b3f55c63
valeriy grigoryev <valeriy.grigoryev@gmail.com>
2017-06-08 00:09:26 +0300

    Выполнил задание Ключ на старт

--- js/game-data.test.js
+++ js/game-data.test.js
@@ -1,3 +1,9 @@
-/**
- * Created by Kleon on 08.06.2017.
- */
+import assert from 'assert';
+
+describe(`Array`, () => {
+  describe(`#indexOf()`, () => {
+    it(`should return -1 when the value is not present`, () => {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
