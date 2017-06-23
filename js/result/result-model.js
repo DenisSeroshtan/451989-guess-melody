3087ade4ca70bbef2322258f91e75e9210f08e3a
valeriy grigoryev <valeriy.grigoryev@gmail.com>
2017-06-23 23:57:13 +0300

    Правки

--- js/result/result-model.js
+++ js/result/result-model.js
@@ -2,16 +2,10 @@ import BaseModel from '../base-model.js';
 import {BaseAdapter} from '../base-model.js';
 
 const resultAdapter = new class extends BaseAdapter {
-  constructor() {
-    super();
-  }
+
 }();
 
 class ResultModel extends BaseModel {
-  constructor() {
-    super();
-  }
-
   send(data) {
     super.send(data, resultAdapter);
   }
