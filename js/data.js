d635da15cb2c75af54bfbda1c06859122e1926f9
valeriy grigoryev <valeriy.grigoryev@gmail.com>
2017-06-25 20:31:28 +0300

    Изменения

--- js/data.js
+++ js/data.js
@@ -3,6 +3,8 @@
  */
 import gameModel from './game/game-model.js';
 
+export const USERNAME = `451989`;
+
 export const gameInfo = Object.freeze({
   'gameName': `Угадай Мелодию`,
   'rules': `Правила просты&nbsp;— за&nbsp;${Math.round(gameModel.initState.time / 60)} минуты дать
